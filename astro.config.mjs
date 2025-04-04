2b63a8ee1e72f5e890413c0cb0b5692ddd6d455c
claywdavis1 <claywdavis1@gmail.com>
2025-04-04 15:24:07 -0800

    changed index and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
+import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
